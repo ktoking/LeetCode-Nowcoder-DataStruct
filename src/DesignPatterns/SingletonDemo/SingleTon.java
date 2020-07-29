2f9ba133384e1955d32c3eff01eceb77c5992de8
ktoking <ktoking@qq.com>
2020-07-29 17:26:47 +0800

    设计模式 单例模式

---
+++ src/DesignPatterns/SingletonDemo/SingleTon.java
@@ -0,0 +1,21 @@
+package DesignPatterns.SingletonDemo;
+
+/**
+ * 单例模式 DCL双端校验锁
+ */
+public class SingleTon {
+    private volatile static SingleTon singleTon;
+    private SingleTon(){};
+
+    public static SingleTon getInstance(){
+        if(singleTon==null){
+            synchronized (SingleTon.class){
+                if(singleTon==null){
+                    singleTon=new SingleTon();
+                }
+            }
+        }
+        return singleTon;
+    }
+}
+
