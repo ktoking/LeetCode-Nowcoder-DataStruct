11c4e9280bdc8a746f8a8f17de97f8800f19900e
ktoking <ktoking@qq.com>
2020-08-04 18:32:19 +0800

    树的子结构问题

---
+++ src/LeetCode/swordToOffer/Offer_8_4_26.java
@@ -0,0 +1,62 @@
+package LeetCode.swordToOffer;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+/**
+ * 剑指 Offer 26. 树的子结构
+ * 输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+ *
+ * B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+ *
+ * 例如:
+ * 给定的树 A:
+ *
+ *      3
+ *     / \
+ *    4   5
+ *   / \
+ *  1   2
+ * 给定的树 B：
+ *
+ *    4
+ *   /
+ *  1
+ * 返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+ *
+ * 示例 1：
+ *
+ * 输入：A = [1,2,3], B = [3,1]
+ * 输出：false
+ * 示例 2：
+ *
+ * 输入：A = [3,4,5,1,2], B = [4,1]
+ * 输出：true
+ * 限制：
+ *
+ * 0 <= 节点个数 <= 10000
+ */
+public class Offer_8_4_26 {
+
+    /**
+     * TODU
+     * @param A
+     * @param B
+     * @return
+     */
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        if (A == null || B == null) return false;
+        return dfs(A, B) || isSubStructure(A.left, B) || isSubStructure(A.right, B);
+    }
+
+    public boolean dfs(TreeNode A, TreeNode B) {
+        if (B == null) return true;
+        if (A == null) return false;
+        return A.val == B.val && dfs(A.left, B.left) && dfs(A.right, B.right);
+    }
+}
