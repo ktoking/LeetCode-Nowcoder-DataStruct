31508b43d1d2984584da18d49c670c6581e8dbfb
ktoking <ktoking@qq.com>
2020-08-21 14:52:05 +0800

    二叉树最小深度

---
+++ src/LeetCode/Test_8_21__111.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+/**
+ * 111. 二叉树的最小深度
+ * 给定一个二叉树，找出其最小深度。
+ *
+ * 最小深度是从根节点到最近叶子节点的最短路径上的节点数量。
+ *
+ * 说明: 叶子节点是指没有子节点的节点。
+ *
+ * 示例:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回它的最小深度  2.
+ */
+public class Test_8_21__111 {
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        int left=minDepth(root.left);
+        int right=minDepth(root.right);
+        return left==0||right==0? 1+right+left:1+Math.min(left,right);
+    }
+
+    public int minDepth1(TreeNode root) {
+        if(root == null) return 0;
+        //这道题递归条件里分为三种情况
+        //1.左孩子和有孩子都为空的情况，说明到达了叶子节点，直接返回1即可
+        if(root.left == null && root.right == null) return 1;
+        //2.如果左孩子和由孩子其中一个为空，那么需要返回比较大的那个孩子的深度
+        int m1 = minDepth(root.left);
+        int m2 = minDepth(root.right);
+        //这里其中一个节点为空，说明m1和m2有一个必然为0，所以可以返回m1 + m2 + 1;
+        if(root.left == null || root.right == null) return m1 + m2 + 1;
+
+        //3.最后一种情况，也就是左右孩子都不为空，返回最小深度+1即可
+        return Math.min(m1,m2) + 1;
+    }
+}
