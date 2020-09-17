7f2f24a7c050c536789b6311ade21c3aafe4aa68
ktoking <1550196694@qq.com>
2020-09-17 20:33:34 +0800

    二叉树层次遍历 递归与非递归

--- src/LeetCode/Test_5_13__102.java
+++ src/LeetCode/Test_5_13__102.java
@@ -1,51 +1,83 @@
-//package LeetCode;
-//
-//import java.util.*;
-//import java.util.concurrent.ArrayBlockingQueue;
-//
-///**
-// *
-// 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
-//
-//
-//
-// 示例：
-// 二叉树：[3,9,20,null,null,15,7],
-//
-// 3
-// / \
-// 9  20
-// /  \
-// 15   7
-// 返回其层次遍历结果：
-//
-// [
-// [3],
-// [9,20],
-// [15,7]
-// ]
-// */
-//class TreeNode {
-//      int val;
-//      TreeNode left;
-//      TreeNode right;
-//      TreeNode(int x) { val = x; }
-//  }
-//public class Test_5_13__102 {
-//    public static void main(String[] args) {
-//        HashMap hashMap=new HashMap();
-//
-//    }
-//
-//
-//
-//    public List<List<Integer>> levelOrder(TreeNode root) {
-//
-//        if(root==null) return new ArrayList<List<Integer>>();
-//
-//        List<List<Integer>> lists=new ArrayList<List<Integer>>();
-//        LinkedList queue=new LinkedList<TreeNode>();
-//        Queue queue1=new LinkedList();
-//
-//    }
-//}
+package LeetCode;
+
+import java.util.*;
+import java.util.concurrent.ArrayBlockingQueue;
+
+/**
+ *
+ 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+
+
+
+ 示例：
+ 二叉树：[3,9,20,null,null,15,7],
+
+ 3
+ / \
+ 9  20
+ /  \
+ 15   7
+ 返回其层次遍历结果：
+
+ [
+ [3],
+ [9,20],
+ [15,7]
+ ]
+ */
+
+public class Test_5_13__102 {
+
+    /**
+     * 层序遍历非递归bfs
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> lists=new ArrayList<>();
+        if(root==null) return lists;
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list=new ArrayList<>();
+            while (size-->0){
+                TreeNode poll = queue.poll();
+                list.add(poll.val);
+                if(poll.left!=null){
+                    queue.offer(poll.left);
+                }
+                if(poll.right!=null){
+                    queue.offer(poll.right);
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+
+    /**
+     * 层序遍历递归dfs
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder1(TreeNode root) {
+        List<List<Integer>> res  = new ArrayList<>();
+        if(root != null){
+            dfs(res, root, 0);
+        }
+        return res;
+    }
+    private void dfs(List<List<Integer>> res, TreeNode node, int level){
+        if(res.size() - 1 < level){ //当遍历到一个新的深度 level，而最终结果 res 中还没有创建 level 对应的列表时，应该在 res 中新建一个列表用来保存该 level 的所有节点。
+            res.add(new ArrayList<Integer>());
+        }
+        res.get(level).add(node.val); //将节点值加入各自所在层
+        if(node.left!=null){
+            dfs(res, node.left, level + 1);
+        }
+        if(node.right!=null){
+            dfs(res, node.right, level + 1);
+        }
+    }
+}
