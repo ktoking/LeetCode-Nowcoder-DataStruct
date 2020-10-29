4a9c95c5193a46665e18e13a7115345b598daade
ktoking <1550196694@qq.com>
2020-10-29 16:04:53 +0800

    tow sum

--- src/LeetCode/Test_5_9__1.java
+++ src/LeetCode/Test_5_9__1.java
@@ -89,4 +89,23 @@ public class Test_5_9__1 {
         throw new IllegalArgumentException("No two sum solution");
     }
 
+
+    /**
+     * 再写一遍
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum111(int[] nums, int target) {
+        Map<Integer,Integer> map=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.containsKey(target-nums[i])){
+                return new int[]{i,map.get(target-nums[i])};
+            }else {
+                map.put(nums[i],i);
+            }
+        }
+        return new int[]{};
+    }
+
 }
