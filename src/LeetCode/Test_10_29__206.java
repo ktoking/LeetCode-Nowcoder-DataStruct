1879075daa6cab51982fd5a1d655fb6e9dd2bab3
ktoking <1550196694@qq.com>
2020-10-29 19:00:55 +0800

    反转链表

---
+++ src/LeetCode/Test_10_29__206.java
@@ -0,0 +1,48 @@
+package LeetCode;
+
+/**
+ * 206. 反转链表
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ */
+public class Test_10_29__206 {
+    /**
+     * 迭代法,头插法而已
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        if(head==null) return null;
+        ListNode temp=head,next=null;
+        ListNode newHead=new ListNode(-1);
+        while (temp!=null){
+            next=temp.next;
+            temp.next=newHead.next;
+            newHead.next=temp;
+            temp=next;
+        }
+        return newHead.next;
+    }
+
+    /**
+     * 递归写法,这大哥太牛了
+     * https://leetcode-cn.com/problems/reverse-linked-list/solution/dong-hua-yan-shi-206-fan-zhuan-lian-biao-by-user74/
+     * @param head
+     * @return
+     */
+    public ListNode reverseList1(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+        ListNode cur=reverseList1(head.next);
+        head.next.next=head;
+        head.next=null;
+        return cur;
+    }
+}
