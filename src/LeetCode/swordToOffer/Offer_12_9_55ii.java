55670200e923a088ac10979096c132461d4f6d47
ktoking <1550196694@qq.com>
2020-12-10 14:07:43 +0800

    剑指 Offer 55 - II. 平衡二叉树

---
+++ src/LeetCode/swordToOffer/Offer_12_9_55ii.java
@@ -0,0 +1,60 @@
+package LeetCode.swordToOffer;
+
+import java.util.Scanner;
+
+/**
+ * 剑指 Offer 55 - II. 平衡二叉树
+ * 输入一棵二叉树的根节点，判断该树是不是平衡二叉树。如果某二叉树中任意节点的左右子树的深度相差不超过1，那么它就是一棵平衡二叉树。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 给定二叉树 [3,9,20,null,null,15,7]
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回 true 。
+ *
+ * 示例 2:
+ *
+ * 给定二叉树 [1,2,2,3,3,null,null,4,4]
+ *
+ *        1
+ *       / \
+ *      2   2
+ *     / \
+ *    3   3
+ *   / \
+ *  4   4
+ * 返回 false 。
+ *
+ *
+ *
+ * 限制：
+ *
+ * 1 <= 树的结点个数 <= 10000
+ * 注意：本题与主站 110 题相同：https://leetcode-cn.com/problems/balanced-binary-tree/
+ */
+public class Offer_12_9_55ii {
+
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) return true; //特值处理
+        if(Math.abs(getHigh(root.left)-getHigh(root.right))<=1){
+            return isBalanced(root.left)&&isBalanced(root.right);
+        }
+        return false;
+    }
+    /**
+     * 找出最大深度
+     * @param root
+     * @return
+     */
+    private int getHigh(TreeNode root){
+        if(root==null) return 0;
+        return Math.max(getHigh(root.left),getHigh(root.right))+1;
+    }
+}
