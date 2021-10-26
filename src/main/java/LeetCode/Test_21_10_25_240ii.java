93d2a49d6c69f21bd36b5b0ec52a94ce521f7e8a
kaiyiwang <kaiyiwang@trip.com>
2021-10-26 10:45:30 +0800

    搜索二维矩阵 二分查找

---
+++ src/main/java/LeetCode/Test_21_10_25_240ii.java
@@ -0,0 +1,53 @@
+package LeetCode;
+
+/**
+ * 240. 搜索二维矩阵 II
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ *
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+ * 输出：true
+ * 示例 2：
+ *
+ *
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+ * 输出：false
+ *
+ *
+ * 提示：
+ *
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= n, m <= 300
+ * -109 <= matrix[i][j] <= 109
+ * 每行的所有元素从左到右升序排列
+ * 每列的所有元素从上到下升序排列
+ * -109 <= target <= 109
+ * 通过次数197,023提交次数401,350
+ */
+public class Test_21_10_25_240ii {
+
+
+    /**
+     * 因为第一行最右边的数 一定比这一行所有的大 又是这一列最小 那直接从右上点开始二分查找即可
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int r = 0, c = n - 1;
+        while (r < m && c >= 0) {
+            if (matrix[r][c] < target) r++;
+            else if (matrix[r][c] > target) c--;
+            else return true;
+        }
+        return false;
+    }
+}
