6056cb79855900da726034933085c9a6c37deac0
kaiyiwang <kaiyiwang@trip.com>
2022-02-07 18:54:08 +0800

    逆序链表相加

--- src/main/java/LeetCode/Test_8_3__2.java
+++ src/main/java/LeetCode/Test_8_3__2.java
@@ -70,4 +70,76 @@ public class Test_8_3__2 {
         }
         return head.next;
     }
+
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2= new ListNode(4);
+        ListNode listNode3 = new ListNode(9);
+        listNode1.next=listNode2;listNode2.next=listNode3;
+        ListNode listNode4 = new ListNode(5);
+        ListNode listNode5 = new ListNode(6);
+        ListNode listNode6 = new ListNode(4);
+        ListNode listNode7 = new ListNode(9);
+        listNode4.next=listNode5;listNode5.next=listNode6;listNode6.next=listNode7;
+
+        addTwoNumbers111(listNode1,listNode4);
+
+    }
+
+    // 逆序做法 大数相加思路
+    public static ListNode addTwoNumbers111(ListNode l1, ListNode l2) {
+        ListNode head=new ListNode(-1);
+        ListNode temp=head;
+        int carry=0;
+        while (l1!=null||l2!=null||carry!=0){
+            if(l1!=null) {
+                carry+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null) {
+                carry+=l2.val;
+                l2=l2.next;
+            }
+            ListNode listNode = new ListNode(carry % 10);
+            carry/=10;
+            temp.next=listNode;
+            temp=temp.next;
+        }
+        return head.next;
+    }
+
+
+    // 不逆序做法
+    public static ListNode addTwoNumbersR(ListNode l1, ListNode l2) {
+        StringBuilder s1=new StringBuilder("");
+        StringBuilder s2=new StringBuilder("");
+        while (l1!=null){
+            s1.append(l1.val);
+            l1=l1.next;
+        }
+        while (l2!=null){
+            s2.append(l2.val);
+            l2=l2.next;
+        }
+
+        int n1=s1.length()-1;
+        int n2=s2.length()-1;
+        int jin=0;
+        StringBuilder res=new StringBuilder("");
+        while (n1>=0||n2>=0||jin!=0){
+            if(n1>=0) jin+=s1.charAt(n1--)-'0';
+            if(n2>=0) jin+=s2.charAt(n2--)-'0';
+            res.append(jin%10);
+            jin/=10;
+        }
+
+        ListNode head = new ListNode(-1);
+        ListNode temp=head;
+        for (int i = 0; i <res.length(); i++) {
+            temp.next=new ListNode(res.charAt(i)-'0');
+            temp= temp.next;
+        }
+        return head.next;
+    }
 }
