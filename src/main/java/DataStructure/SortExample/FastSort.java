99e3fa56c8732a348003f6ace2cc5bb5e0274a48
kaiyiwang <kaiyiwang@trip.com>
2022-02-22 15:37:13 +0800

    快排杀我

--- src/main/java/DataStructure/SortExample/FastSort.java
+++ src/main/java/DataStructure/SortExample/FastSort.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class FastSort {
     public static void main(String[] args) {
         int[] arr={2,3,1,2,3,5,6,2,1};
-//        fast(0,arr.length-1,arr);
+        fast(0,arr.length-1,arr);
         fastS(arr,0,arr.length-1);
+        fast1(0, arr.length-1,arr);
         Arrays.stream(arr).forEach(value -> System.out.println(value));
     }
     public static void fast(int low,int high,int[] arr){
@@ -56,17 +57,25 @@ public class FastSort {
 
 
 
+    public static void fast1(int low,int high,int[] arr){
+        if(low<high){
+            int index = sort1(low, high, arr);
+            fast1(low,index-1,arr);
+            fast1(index+1,high,arr);
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
+    public static int sort1(int low,int high,int[] arr){
+        int temp=arr[low];
+        while (low<high){
+            while (low<high&&arr[high]>=temp) high--;
+            arr[low]=arr[high];
+            while (low<high&&arr[low]<=temp) low++;
+            arr[high]=arr[low];
+        }
+        arr[low]=temp;
+        return low;
+    }
 
 
 }
