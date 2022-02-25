a16dfe043dbb9e3b4f6f34954f59a3e472e7f9da
kaiyiwang <kaiyiwang@trip.com>
2022-02-25 21:08:24 +0800

    最小路径和

--- src/main/java/LeetCode/Test_7_23__64.java
+++ src/main/java/LeetCode/Test_7_23__64.java
@@ -59,4 +59,40 @@ public class Test_7_23__64 {
         }
         return dp[row-1][col-1];
     }
+
+    /**
+     * dp
+     * @param grid
+     * @return
+     */
+    public int minPathSum2(int[][] grid) {
+        if(grid.length==0) return 0;
+        int[][] dp= new int[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if(i==0&&j==0) dp[i][j]=grid[i][j];
+                else if(i==0) dp[i][j]=grid[i][j]+dp[i][j-1];
+                else if(j==0) dp[i][j]=grid[i][j]+dp[i-1][j];
+                else dp[i][j]=Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+            }
+        }
+        return dp[grid.length-1][grid[0].length-1];
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
